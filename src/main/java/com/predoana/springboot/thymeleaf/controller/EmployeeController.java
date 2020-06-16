643bad259d4557783ca7edd890d70b3d3d507a06
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-16 23:57:02 +0300

    Constructor injection

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import com.predoana.springboot.thymeleaf.entity.Employee;
+import com.predoana.springboot.thymeleaf.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+	private EmployeeService employeeService;
+
+	public EmployeeController (EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
 	
 	//add mapping for "/list"
 	
