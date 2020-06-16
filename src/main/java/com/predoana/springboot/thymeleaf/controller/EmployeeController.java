b76ff0e1c760047bee5064a78b1177800492e6a4
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-16 23:40:24 +0300

    Create the "entity" package

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import com.predoana.springboot.thymeleaf.entity.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.predoana.springboot.thymeleaf.model.Employee;
 
 @Controller
 @RequestMapping("/employees")
