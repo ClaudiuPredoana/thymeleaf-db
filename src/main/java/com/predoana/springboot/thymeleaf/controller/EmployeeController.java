be057becbcaafc6ac07a348a0c25b51060f8c8b8
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-16 23:52:38 +0300

    Remove the in-memory employee code

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -15,23 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	@PostConstruct
-	private void loadData() {
-		
-		//create employees
-		Employee emp1 = new Employee(1, "Laura", "Andre", "laura@gmail.com");
-		Employee emp2 = new Employee(2, "Ema", "Berechet", "ema@gmail.com");
-		Employee emp3 = new Employee(3, "Arman", "Grigore", "arman@gmail.com");
-		
-		//create the list
-		theEmployees = new ArrayList<>();
-		
-		//add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-		
-	}
 	
 	//add mapping for "/list"
 	
